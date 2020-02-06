50c105e4b9a889d6b92850fe150fc1b3b8b85a6f
emedee <emediongini@gmail.com>
2020-02-06 05:01:24 -1200

    'commit'

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,8 @@
 <template>
-   <div class="myCenter">
+   <div>
         <div class="row justify-content-center">
             <div class="col-sm-5">
-                <div class="card shadow p-4">
+                <div class="card shadow p-4 myCenter">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="well">
@@ -22,7 +22,7 @@
                         <div class="col-md-12 mt-5" v-if="toggle">
                             <center><h2>Things to be done</h2></center>
                             <ul class="list-group">
-                                <li v-for="(todo, index) in todos" :key="index" class="list-group-item">
+                                <li v-for="(todo, index) in todos" :key="index" class="list-group-item black">
                                     <p>{{ index + 1 }}- <span @click="completedTasks(index)" class="myHov" >{{ todo }}</span>
                                     <span @click="deleteTodos(index)"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></span>
                                     <span class="mys mr-2" @click="editTodos(todo)">Edit</span></p>       
@@ -33,7 +33,7 @@
                         <div class="col-md-12 mt-5" v-if="secondToggle">
                             <center><h2>Completed Tasks</h2></center>
                             <ul class="list-group">
-                                <li v-for="(complete, i) in completed" :key="i" class="list-group-item">
+                                <li v-for="(complete, i) in completed" :key="i" class="list-group-item black">
                                     <p>{{ i + 1 }}- <span class="myHov" >{{ complete }}</span></p>       
                                 </li>
                             </ul>
@@ -124,6 +124,13 @@ export default {
 }
 .mys:hover{
     cursor: pointer;
+}
+.myCenter{
+    background-color: black;
+    color: white;
+}
+.black{
+    color: black !important;
 }
 @media (max-width: 575.98px) {
     span{
