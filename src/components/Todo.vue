84f08a5c76b002f60b1c226963bf9ae47c9955b4
emedee <emediongini@gmail.com>
2020-02-06 03:57:03 -1200

    'commit'

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -13,27 +13,28 @@
                                         <input type="text" v-model="myList.input" class="form-control" placeholder="E.g Build a web app" />
                                     </div>
                                     <button type="button" v-if="display" v-on:click="update()" class="btn btn-secondary">Update</button>
-                                    <button v-else type="button" v-on:click="addTodos()" class="btn btn-primary">Create</button>
+                                    <button  v-else type="button" v-on:click="addTodos()" class="btn btn-primary">Create</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-12 mt-5">
-                            <center><h2>ToDos</h2></center>
+                        <div class="col-md-12 mt-5" v-if="toggle">
+                            <center><h2>Things to be done</h2></center>
                             <ul class="list-group">
                                 <li v-for="(todo, index) in todos" :key="index" class="list-group-item">
-                                    <p>{{ index + 1 }}- <span @click="strikeThrough(index)" class="myHov" >{{ todo }}</span>
+                                    <p>{{ index + 1 }}- <span @click="completedTasks(index)" class="myHov" >{{ todo }}</span>
                                     <span @click="deleteTodos(index)"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></span>
                                     <span class="mys mr-2" @click="editTodos(todo)">Edit</span></p>       
                                 </li>
                             </ul>
+                            <center><p><strong>Click on a task after completion</strong></p></center>
                         </div>
-                        <div class="col-md-12 mt-5">
-                            <center><h2>Done</h2></center>
+                        <div class="col-md-12 mt-5" v-if="secondToggle">
+                            <center><h2>Completed Tasks</h2></center>
                             <ul class="list-group">
                                 <li v-for="(complete, i) in completed" :key="i" class="list-group-item">
-                                    <p>{{ i + 1 }}- <span @click="strikeThrough(i)" class="myHov" >{{ complete }}</span></p>       
+                                    <p>{{ i + 1 }}- <span class="myHov" >{{ complete }}</span></p>       
                                 </li>
                             </ul>
                         </div>
@@ -55,6 +56,8 @@ export default {
 
                 completed: [],
                 display: false,
+                toggle: false,
+                secondToggle: false,
                 todoToUpdate: null,
                 editField: null,
                 
@@ -69,6 +72,7 @@ export default {
                         text: 'Please insert a to-do',
                     })
                 }else{
+                    this.toggle = true
                     this.todos.push(this.myList.input);
                     this.myList.input = "";
                 }
@@ -92,7 +96,8 @@ export default {
                 this.todos[this.todoToUpdate] = this.myList.input;
                 this.myList.input = "";
             },
-            strikeThrough(i){
+            completedTasks(i){
+                this.secondToggle = true;
                 console.log(this.todos[i])
                 this.completed.push(this.todos[i]);
                 this.todos.splice(i, 1)
