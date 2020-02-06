e9f9d9c72e9ebe998d574f8d3381919b417f79fc
emedee <emediongini@gmail.com>
2020-02-06 01:15:41 -1200

    'commit'

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,32 +1,4 @@
 <template>
-    <!-- <div class="container to-do">
-        <div class="container mt-5">
-            <h1>todos!</h1>
-            <p class="p1">Add you first to-do</p>
-            <img src="/src/assets/logo.jpg" alt="">
-            <p class="p2">What do you want to get done today?</p>
-            <input type="text" name="" id="" placeholder="E.g Build a web app" class="myInp mr-3"><button @click="addMyTodos" type="button" class="btn btn-primary">Submit</button>
-        </div>
-
-        <div class="container marg">
-            <table class="table table-borderless">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">todos</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(myTodo,i) in myTodos">
-                    <th scope="row">{{parseInt(i) + 1}}</th>
-                    <td>{{myTodo.item}}</td>
-                    <td><button type="button" class="btn btn-secondary mr-3">Edit To-Do</button><button type="button" class="btn btn-danger">Delete To-Do</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div> -->
-
    <div class="myCenter">
         <div class="row justify-content-center">
             <div class="col-sm-5">
@@ -40,7 +12,7 @@
                                         <label for="todoitem">What do you want to get done today??</label>
                                         <input type="text" v-model="myList.input" class="form-control" placeholder="E.g Build a web app" />
                                     </div>
-                                    <button type="button" v-if="display" v-on:click="update" class="btn btn-secondary">Update</button>
+                                    <button type="button" v-if="display" v-on:click="update()" class="btn btn-secondary">Update</button>
                                     <button v-else type="button" v-on:click="addTodos()" class="btn btn-primary">Create</button>
                                 </form>
                             </div>
@@ -48,12 +20,20 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12 mt-5">
-                            <h2>ToDos</h2>
+                            <center><h2>ToDos</h2></center>
                             <ul class="list-group">
                                 <li v-for="(todo, index) in todos" :key="index" class="list-group-item">
                                     <p>{{ index + 1 }}- <span @click="strikeThrough(index)" class="myHov" >{{ todo }}</span>
                                     <span @click="deleteTodos(index)"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></span>
                                     <span class="mys mr-2" @click="editTodos(todo)">Edit</span></p>       
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-12 mt-5">
+                            <center><h2>Done</h2></center>
+                            <ul class="list-group">
+                                <li v-for="(complete, i) in completed" :key="i" class="list-group-item">
+                                    <p>{{ i + 1 }}- <span @click="strikeThrough(i)" class="myHov" >{{ complete }}</span></p>       
                                 </li>
                             </ul>
                         </div>
@@ -72,14 +52,24 @@ export default {
                     input: "",
                 },
                 todos: [],
+
+                completed: [],
                 display: false,
-                completed: true,
+                
             }
         },
         methods: {
             addTodos() {
-                this.todos.push(this.myList.input);
-                this.myList.input = "";
+                if(this.myList.input === ''){
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Please insert a to-do',
+                    })
+                }else{
+                    this.todos.push(this.myList.input);
+                    this.myList.input = "";
+                }
             },
             deleteTodos(index){
                 this.todos.splice(index, 1)
@@ -87,6 +77,7 @@ export default {
             editTodos(id){
                 this.myList.input = id
                 this.display = true
+                console.log(this.myList.input)
             },
             update(){
                 Swal.fire(
@@ -97,8 +88,10 @@ export default {
                 this.display = false;
                 this.myList.input = "";
             },
-            strikeThrough(index){
-                this.completed = false;
+            strikeThrough(i){
+                console.log(this.todos[i])
+                this.completed.push(this.todos[i]);
+                this.todos.splice(i, 1)
             },
         }
 }
@@ -123,7 +116,13 @@ export default {
 .mys:hover{
     cursor: pointer;
 }
-@media screen {
-    
+@media (max-width: 575.98px) {
+    span{
+        font-size: 15px !important;
+    }
+    .mys{
+        font-size: 15px !important;
+        padding: 5px !important;
+    }
 }
 </style>
